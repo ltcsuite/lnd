8d54b26fdcee178cb2b8b184e28e808d61297665
Hector Chu <hectorchu@gmail.com>
2024-02-16 09:37:06 +0000

    Upgraded transaction detail

--- lnrpc/marshall_utils.go
+++ lnrpc/marshall_utils.go
@@ -169,6 +169,8 @@ func MarshallOutputType(o txscript.ScriptClass) OutputScriptType {
 		return OutputScriptType_SCRIPT_TYPE_WITNESS_UNKNOWN
 	case txscript.WitnessV1TaprootTy:
 		return OutputScriptType_SCRIPT_TYPE_WITNESS_V1_TAPROOT
+	case txscript.MwebTy:
+		return OutputScriptType_SCRIPT_TYPE_MWEB
 	default:
 		return OutputScriptType_SCRIPT_TYPE_PUBKEY_HASH
 	}
